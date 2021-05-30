bde3fcf084605604c3c8c3e075a053ca32453f29
leorodrigues10 <leonelmiranda10@outlook.com>
2021-05-30 22:34:19 -0100

    add social links

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,40 +3,44 @@ import leo from '../../assets/img/leo.png'
 function Home() {
   return (
     <div className="home-container">
-      <section className="social">
-        <ul className="social-list">
-          <li className="social-item">
-            <a href="https://web.facebook.com/leonelove.miranda/" target="_blank" rel="noreferrer" className="social-link">
-              <i class="fab fa-facebook-f fa-lg"></i>
-            </a>
-          </li>
-          <li className="social-item">
-            <a className="social-link fa-lg">
-              <i class="fab fa-github"></i>
-            </a>
-          </li>
-          <li className="social-item">
-            <a className="social-link ">
-              <i class="fab fa-linkedin-in fa-lg"></i>
-            </a>
-          </li>
-        </ul>
-      </section>
-      <section className="aboutme">
-        <div>
-          <span className="init">Hi, my name is</span><br /><span className="bold">Américo Silva</span>
-          <span className="init">, i'm a </span><br /> <span className="bold">FullStack Developer</span>
-          <p className="more">&lt;If you need good energies and good vibes, count on me _&gt;<br />
-             I'm always looking for new challenges. I always try to grow, 
-             <br /> to improve myself and to be better than I was yesterday.
-             <br />I am a problem-solver who takes pride in creating innovative 
-             <br /> solutions to solve problems.</p>
-        </div>
-      </section>
+      <div id="about-me" className="about">
 
-      <section className="my-image">
-          <img className="img" src={leo} alt="my-img"/>
-      </section>
+
+        <section className="social">
+          <ul className="social-list">
+            <li className="social-item">
+              <a href="https://web.facebook.com/leonelove.miranda/" target="_blank" rel="noreferrer" className="social-link">
+                <i class="fab fa-facebook-f fa-lg"></i>
+              </a>
+            </li>
+            <li className="social-item">
+              <a href="https://github.com/leorodrigues10" target="_blank" rel="noreferrer" className="social-link fa-lg">
+                <i class="fab fa-github"></i>
+              </a>
+            </li>
+            <li className="social-item">
+              <a href="https://www.linkedin.com/in/leomiradev/" target="_blank" rel="noreferrer" className="social-link ">
+                <i class="fab fa-linkedin-in fa-lg"></i>
+              </a>
+            </li>
+          </ul>
+        </section>
+        <section className="aboutme">
+          <div>
+            <span className="init">Hi, my name is</span><br /><span className="bold">Américo Silva</span>
+            <span className="init">, i'm a </span><br /> <span className="bold">FullStack Developer</span>
+            <p className="more">&lt;If you need good energies and good vibes, count on me _&gt;
+            I'm always looking for new challenges. I always try to grow,
+            to improve myself and to be better than I was yesterday.
+            I am a problem-solver who takes pride in creating innovative
+               solutions to solve problems.</p>
+          </div>
+        </section>
+
+        <section className="my-image">
+          <img className="img" src={leo} alt="my-img" />
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -6,16 +6,16 @@ function Nav(props) {
     <nav className={props.name} style={props.showNav ? navStyle : {}}>
       <ul className="list">
         <li className="nav-list">
-          <a className="nav-link">About me</a>
+          <a href="#about-me" className="nav-link">About me</a>
         </li>
         <li className="nav-list">
-          <a className="nav-link">Projects</a>
+          <a href="#projects" className="nav-link">Projects</a>
         </li>
         <li className="nav-list">
-          <a className="nav-link">Skills</a>
+          <a href="#about-me" className="nav-link">Skills</a>
         </li>
         <li className="nav-list">
-          <a className="nav-link">Contacts</a>
+          <a  href="#about-me"className="nav-link">Contacts</a>
         </li>
       </ul>
     </nav>
